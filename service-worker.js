1c0888d1e7b3fa186c09e19f5eb3fa37570d196e
DN_Physics <eduhub071302@gmail.com>
2026-03-28 03:35:43 +0530

    Update service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = "dn-focus-music-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./css/style.css",
+  "./js/app.js",
+  "./data/tracks.json",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return (
+        cached ||
+        fetch(event.request).catch(() => {
+          return caches.match("./index.html");
+        })
+      );
+    })
+  );
+});
